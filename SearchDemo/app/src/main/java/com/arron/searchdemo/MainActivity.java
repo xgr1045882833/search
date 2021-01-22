eb58b96477cc8aa0af85a0d60940696ca5c97ac6
xgr1045882833 <1045882833@qq.com>
2021-01-22 10:21:24 +0800

    删除多余的注释

--- SearchDemo/app/src/main/java/com/arron/searchdemo/MainActivity.java
+++ SearchDemo/app/src/main/java/com/arron/searchdemo/MainActivity.java
@@ -9,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test01
-        //test02
-        //test03
-        //test04
-        //test05
-        //test06
-        //test07
-        //test08
+
     }
 }
