73e8cb2bba858ca1cf8b7cb446678b98bae366e6
xgr1045882833 <1045882833@qq.com>
2021-01-26 23:14:25 +0800

    新增单元测试文件

---
+++ SearchDemo/app/src/test/java/com/arron/searchdemo/StringTest.java
@@ -0,0 +1,21 @@
+package com.arron.searchdemo;
+
+import org.junit.Test;
+
+/**
+ * create by Aaron Xie
+ * on 2021/1/25
+ * description:
+ */
+public class StringTest {
+
+    @Test
+    public void test() {
+        String string = "adjaljdlajlf";
+        String substring = string.substring(5);
+        System.out.println(substring);
+        String substring1 = string.substring(1, 3);
+        System.out.println(substring1);
+    }
+
+}
